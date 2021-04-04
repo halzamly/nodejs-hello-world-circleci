f5d6e81d05c5bcac392fe27c14e35c0c99abbb13
Hossam Alzamly <hossam.alzamly@operaize.com>
2021-04-04 22:50:29 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
+
+console.log("Server running at http://localhost:%d", port);
